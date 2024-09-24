fda332f06ea8fdf59872c57d9740a4d5588ba33a
sofiapantas <sofiasarabia0831@gmail.com>
2024-09-25 01:32:39 +0800

    bom file

---
+++ week02/scripts/bom.js
@@ -0,0 +1,13 @@
+const input = document.querySelector(#fav-chmps);
+const button = document.querySelector();
+const  = document.querySelector();
+
+const li = document.createElement("li");
+
+const deleteButton = document.createElement("button")
+
+li.textContent = input.value;
+
+deleteButton. textContent = "X";
+
+li.append(deleteButton)
