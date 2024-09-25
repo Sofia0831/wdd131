cd56df6a87527d30bd6753329968e767fe37ff3c
sofiapantas <sofiasarabia0831@gmail.com>
2024-09-25 13:25:26 +0800

    event handling activity

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,14 +2,41 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector('button');
 const list = document.querySelector("#list");
 
-const li = document.createElement("li");
+// const li = document.createElement("li");
 
-const deleteButton = document.createElement("button")
+// const deleteButton = document.createElement("button")
 
-li.textContent = input.value;
+// li.textContent = input.value;
 
-deleteButton.textContent = "X";
+// deleteButton.textContent = "X";
 
-li.append(deleteButton);
+// li.append(deleteButton);
 
-list.append(li);
+// list.append(li);
+
+button.addEventListener("click",
+    function() {
+     const inputValue = input.value.trim();
+   
+     if (inputValue !== '') {
+       const li = document.createElement("li");
+       li.textContent = inputValue;
+   
+       const deleteButton = document.createElement("button");
+       deleteButton.textContent = "❌";
+   
+       deleteButton.addEventListener("click", function() {
+         li.remove();
+         input.focus();
+       });
+   
+       li.appendChild(deleteButton);
+       list.appendChild(li);
+   
+       input.value = '';
+     } else {
+       alert("Please enter a chapter name.");
+       input.focus();
+     }
+   });
+
