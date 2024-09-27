9ec1ea7c5fb956c43e64fbc2f5552e26abeee4e0
sofiapantas <sofiasarabia0831@gmail.com>
2024-09-27 12:16:03 +0800

    added getdate in js

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -5,3 +5,11 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+const currentYear = new Date().getFullYear();
+
+const lastModifiedDate = document.lastModified;
+
+const footerParagraphs = document.querySelector("footer").querySelectorAll("p");
+footerParagraphs[0].textContent = `©️ ${currentYear} 🎭 Sofia Florylle S. Pantas 🎭 Philippines`;
+footerParagraphs[1].textContent = `Last modified: ${lastModifiedDate}`;
