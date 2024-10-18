a96e7cd1a75e150b09513c72e9dcc6dc8bdd252a
sofiapantas <sofiasarabia0831@gmail.com>
2024-10-18 14:09:00 +0800

    form assignment

---
+++ scripts/form.js
@@ -0,0 +1,37 @@
+
+const currentYear = new Date().getFullYear();
+
+const lastModifiedDate = document.lastModified;
+
+const footerParagraphs = document.querySelector("footer").querySelectorAll("p");
+footerParagraphs[0].textContent = `©️${currentYear} 🎭 Sofia Florylle S. Pantas 🎭 Philippines`;
+footerParagraphs[1].textContent = `Last modified: ${lastModifiedDate}`;
+
+
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
