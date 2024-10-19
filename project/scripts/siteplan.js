7096272288f660185ffcede5cc10cb4dd383539f
sofiapantas <sofiasarabia0831@gmail.com>
2024-10-20 03:49:12 +0800

    project site plan

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,7 @@
+const currentYear = new Date().getFullYear();
+
+const lastModifiedDate = document.lastModified;
+
+const footerParagraphs = document.querySelector("footer").querySelectorAll("p");
+footerParagraphs[0].textContent = `©️${currentYear} 🌌  Sofia Florylle S. Pantas 🌌  Philippines`;
+footerParagraphs[1].textContent = `Last modified: ${lastModifiedDate}`;
