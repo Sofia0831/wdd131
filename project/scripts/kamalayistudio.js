1d83487b32ceeb11040c914932978e1119f3a780
sofiapantas <sofiasarabia0831@gmail.com>
2024-10-24 01:04:35 +0800

    sliding animation for socials

--- project/scripts/kamalayistudio.js
+++ project/scripts/kamalayistudio.js
@@ -13,3 +13,9 @@ const lastModifiedDate = document.lastModified;
 const footerParagraphs = document.querySelector("footer").querySelectorAll("p");
 footerParagraphs[0].textContent = `Copyright ©️ ${currentYear} KAMALAYI STUDIOS. Philippines`;
 footerParagraphs[1].textContent = `Last modified: ${lastModifiedDate}`;
+
+
+const image = document.querySelector('.socials');
+image.addEventListener('animationend', () => {
+image.classList.add('slide-in');
+});
