205a4ab1d5fab008ab05290534c9a1017c6bca0a
sofiapantas <sofiasarabia0831@gmail.com>
2024-10-25 23:26:51 +0800

    changed profile pics path

--- project/scripts/profiles.js
+++ project/scripts/profiles.js
@@ -20,17 +20,17 @@ const profiles = [
 	{
 		profileName: "MikoCappuccino",
 		titleName: "Artist Corner",
-		imageUrl: "https://cdn.discordapp.com/attachments/1237341719935193118/1299051416316411925/mikosprite.png?ex=671bcb68&is=671a79e8&hm=3c1d71688f4792f433ef110b2272b3ecdd60d363af1b0e5f6ce3ea75672b11ec&"
+		imageUrl: "images/mikosprite.png"
 	},
 	{
 		profileName: "Puddinheadart",
 		titleName:"Computer Room",
-		imageUrl: "https://cdn.discordapp.com/attachments/1237341719935193118/1299051416555618325/puddinsprite.png?ex=671bcb68&is=671a79e8&hm=9e670a18d459aa643a64113bb604da755d1d105eb86a3c7a294d7889da5ba93d&"
+		imageUrl: "images/puddinsprite.png"
 	},
 	{
 		profileName: "Yui",
 		titleName: "Writing Desk",
-		imageUrl: "https://cdn.discordapp.com/attachments/1237341719935193118/1299051416085856346/yuisprite.png?ex=671bcb68&is=671a79e8&hm=f30b9e0aff0086818363c49a755756b5521b2d72fc544c533c24dac337f42f56&"
+		imageUrl: "images/yuisprite.png"
 	}
 ];
 
